dc7cb84f0ad3f840a0aef15044d034ef0f375bbb
Skite <Skite@mail.ithome.com.tw>
2019-07-11 16:48:18 +0800

    update confapi id

--- js/confapi.js
+++ js/confapi.js
@@ -3,7 +3,7 @@ var confapi = confapi || (function() {
     var Domain = 'https://confapi.ithome.com.tw';
     var currentData = document.currentScript || {};
     currentData = currentData.dataset || {};
-    var NID = currentData.nid || 3531;
+    var NID = currentData.nid || 6270;
     var LOG = currentData.log || false;
     var CACHE = {};
     // private method
